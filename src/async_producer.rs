11386d75a351db806742cc5962468235f5662c73
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2025-01-06 23:12:34 +0100

    loop and sleep on Queue full error

--- src/async_producer.rs
+++ src/async_producer.rs
@@ -1,4 +1,9 @@
+use log::error;
 use log::info;
+use log::warn;
+use rdkafka::types::RDKafkaErrorCode;
+use std::thread;
+use std::time;
 use std::time::Duration;
 use std::time::Instant;
 
@@ -7,7 +12,7 @@ use rdkafka::producer::{
     BaseRecord, DefaultProducerContext, NoCustomPartitioner, Producer, ThreadedProducer,
 };
 
-const FLUSH_TIME_SECS: u64 = 20;
+const FLUSH_TIME_SECS: u64 = 30;
 
 pub async fn produce(
     brokers: &str,
@@ -19,18 +24,34 @@ pub async fn produce(
     let producer: &ThreadedProducer<DefaultProducerContext, NoCustomPartitioner> =
         &ClientConfig::new()
             .set("bootstrap.servers", brokers)
-            .set("message.timeout.ms", "5000")
+            .set("message.timeout.ms", "15000")
             .create()
             .expect("Producer creation error");
 
+    info!("publishing {:?} messages", message_count);
+    let before = Instant::now();
+
     for _ in 0..message_count {
-        let _ = producer.send(BaseRecord::to(topic_name).payload(message).key(key));
+        loop {
+            match producer.send(BaseRecord::to(topic_name).key(key).payload(message)) {
+                Ok(_) => {
+                    break;
+                }
+                Err((err, _record)) => {
+                    if err.rdkafka_error_code().unwrap() == RDKafkaErrorCode::QueueFull {
+                        // warn!("Queue full, retrying...");
+                        thread::sleep(time::Duration::from_millis(200));
+                    } else {
+                        error!("Failed to enqueue message: {:?}", err);
+                        break; // Exit the loop on other errors
+                    }
+                }
+            }
+        }
     }
     producer
         .flush(Duration::from_secs(FLUSH_TIME_SECS))
         .unwrap();
-
-    let before = Instant::now();
 
     info!(
         "finished publishing {} events in {:?} ",
